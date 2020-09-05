2c3544a2dadaeb35512a9e343ef9f44c87df97a5
Christian <christian.dehek@gmail.com>
2020-09-05 08:57:26 -0600

    added search by featured repo buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,12 +2,13 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var response = fetch("https://api.github.com/users/octocat/repos").then(function(response) {
     response.json().then(function(data) {
         console.log(data);
     });
-// });
+});
 console.log(response);
 
 var getUserRepos = function(user) {
@@ -30,6 +31,30 @@ var getUserRepos = function(user) {
           // Notice this .catch getting chained onto the end of the .then method
           alert("Unable to connect to GitHub");
       });
+};
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+        });
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    });
+};
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    if (language) {
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = "";
+    };
 };
 
 var formSubmitHandler = function(event) {
@@ -94,3 +119,4 @@ var displayRepos = function(repos, searchTerm) {
 }
 
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
